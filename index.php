d42bc40e068cfa4add4fbe418c913152f42a397b
Louis Gill <lgill7@cnm.edu>
2015-10-13 10:59:25 -0600

    things I dig

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 		<title>About Louis</title>
 	</head>
 	<body>
-			<p>Yo</p>
+			<p>Yo, these are some things I dig.</p>
+		<ul>
+			<li>Pizza</li>
+			<li>Rainbows</li>
+		</ul>
 		<img src="http://amishpolebarns.com/wp-content/uploads/2012/12/wood-slider-background.jpg" alt="wood"/>
 	</body>
 </html>
