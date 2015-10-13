ed75219029dc5007d67b9d14c2f458d9bb4d6702
Louis Gill <lgill7@cnm.edu>
2015-10-13 10:52:03 -0600

    Yo + image

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 		<title>About Louis</title>
 	</head>
 	<body>
-		<p>Yo</p>
+		<main>
+			<p>Yo</p>
+		</main>
 		<img src="http://amishpolebarns.com/wp-content/uploads/2012/12/wood-slider-background.jpg" />
 	</body>
 </html>
