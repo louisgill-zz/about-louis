9c3d2cea281205e9f3d5cb19637c68638d38e4c5
Louis Gill <lgill7@cnm.edu>
2015-10-13 11:34:48 -0600

    emphashized "Louis Gill"

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="about-louis.css" title="style">
 	</head>
 	<body>
-		<header>The one, the only, Louis Gill</header>
+		<header>The one, the only, <em>Louis Gill</em></header>
 			<p>Yo, these are some things I dig:</p>
 		<ul>
 			<li>Pizza</li>
